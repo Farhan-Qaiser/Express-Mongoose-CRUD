af84c29afa59910ef93bc9fc6faeab6966ae1d58
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-10-09 12:20:29 +0500

    server.listen

--- server.mjs
+++ server.mjs
@@ -70,3 +70,10 @@ app.delete('/users/:id', async (req, res) => {
     res.status(500).json({ message: err.message })
   }
 })
+
+
+
+const PORT =  3000
+app.listen(PORT, () => {
+  console.log(`Server is running on: http://localhost:${PORT}/users`)
+})
