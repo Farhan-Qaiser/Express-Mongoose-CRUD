a2f603e6c68fb8611a3e93377d2678041df03c2d
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-10-09 12:19:01 +0500

    DELETE request added and used .findByIdAndDelete() method to delete item from  DB

--- server.mjs
+++ server.mjs
@@ -59,3 +59,14 @@ app.put('/users/:id', async (req, res) => {
     res.status(400).json({ message: err.message })
   }
 })
+
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id)
+    if (!deletedUser) return res.status(404).json({ message: 'User not found' })
+    res.status(200).json({ message: 'User deleted' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
