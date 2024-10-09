91c32cb20f1aba2de358f11e63839b18a4e13a90
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-10-09 12:12:19 +0500

    mongo URI added and connected server to mongoose

--- server.mjs
+++ server.mjs
@@ -6,3 +6,10 @@ import cors from "cors";
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+
+const mongoURI = 'mongodb://localhost:27017/users_management'
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err))
