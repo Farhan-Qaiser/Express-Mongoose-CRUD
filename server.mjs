f325c2b5eb1c6f36cdcdb41b0232aede0da8d8a8
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-10-09 12:16:20 +0500

    POST request added and used .save() method to add new item to DB

--- server.mjs
+++ server.mjs
@@ -33,3 +33,14 @@ app.get('/users', async (req, res) => {
     res.status(500).json({ message: err.message })
   }
 })
+
+
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body)
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
